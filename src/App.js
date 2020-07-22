f505ee2ff9d828e067963c758492cb90ec35b910
tessssssssy <tessfabry@gmail.com>
2020-07-22 21:57:47 +1000

    add links

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 import { searchRepos } from './actions/repos'
 import Card from './Card'
 
-class App extends React.Component {
+class App extends Component {
   state = { search: "" }
-  
+
   searchGithub = async (search) => {
     const response = await fetch(`https://api.github.com/search/repositories?q=${search}`);
     const results = await response.json();
@@ -14,19 +14,20 @@ class App extends React.Component {
     this.props.dispatch(searchRepos(results))
   }
 
-  onInputChange = (e) => {
-    if (e.target.value.length > 2) {
-      this.setState({[e.target.name]: e.target.value})
-      this.searchGithub(this.state.search)
+  onInputChange = (e) => { 
+    if (e.target.value.length > 2) {   
+      this.searchGithub(e.target.value)
+      this.setState({[e.target.id]: e.target.value})
     } 
   }
 
   render() {
+    console.log(this.props.repos)
     return (    
       <div className="App">
-        <input type="text" name="search" onChange={this.onInputChange}></input>
-        {this.props.repos && this.props.repos.items.map((repo) => {
-          return <Card name={repo.name} stargazers_count={repo.stargazers_count} watchers_count={repo.watchers_count}/>
+        <input type="text" id="search" onChange={this.onInputChange}></input>
+        {this.props.repos && this.props.repos.results.items.map((repo, index) => {
+          return <Card key={index} name={repo.name} stargazersCount={repo.stargazers_count} watchersCount={repo.watchers_count} url={repo.svn_url}/>
         }) }
       </div>
     );
--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Card = (props) => {
     return (
         <div className="card">
-            <h1>{props.name}</h1>
+            <a href={props.url} target="_blank">{props.name}</a>
+            <p>Stargazers: {props.stargazersCount}</p>
+            <p>Watchers: {props.watchersCount}</p>
         </div>
     )
 }
